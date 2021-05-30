5c7797520612eb888c54b30dfad7d71e4fc43dbc
Ahmed Eid <a.eid@yandex.com>
2021-05-30 07:21:06 +0200

    testing 16.

--- android/src/main/java/com/reactnativepaymob/PaymobModule.java
+++ android/src/main/java/com/reactnativepaymob/PaymobModule.java
@@ -8,6 +8,12 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 
+ import com.paymob.acceptsdk.PayActivity;
+ import com.paymob.acceptsdk.PayActivityIntentKeys;
+ import com.paymob.acceptsdk.PayResponseKeys;
+ import com.paymob.acceptsdk.SaveCardResponseKeys;
+ import com.paymob.acceptsdk.ToastMaker;
+
 @ReactModule(name = PaymobModule.NAME)
 public class PaymobModule extends ReactContextBaseJavaModule {
     public static final String NAME = "Paymob";
