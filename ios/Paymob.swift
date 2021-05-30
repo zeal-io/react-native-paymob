4453c0589df3a6cf931e22ca073a32f6e98f5ea4
Ahmed Eid <a.eid@yandex.com>
2021-05-30 05:27:02 +0200

    testing 3.

--- ios/Paymob.swift
+++ ios/Paymob.swift
@@ -92,128 +92,38 @@ class Paymob: UIViewController, AcceptSDKDelegate {
                                     isEnglish: mappedData["isEnglish"] as! Bool
             )
 
-//                                    saveCardDefault: json["saveCardDefault"],
-//                                    showSaveCard: json["showSaveCard"],
-//                                    showAlerts: json["showAlerts"],
-//                                    token: json["token"],
-//                                    maskedPanNumber: json["maskedPanNumber"],
-//                                    buttonsColor: json["buttonsColor"],
-//                                    isEnglish: json["isEnglish"],
-//                                    backgroundColor: json["backgroundColor"],
-//                                    navBarColor: json["navBarColor"],
-//                                    navBarTextColor: json["navBarTextColor"],
-//                                    textFieldBackgroundColor: json["textFieldBackgroundColor"],
-//                                    textFieldTextColor: json["textFieldTextColor"],
-//                                    titleLabelTextColor: json["titleLabelTextColor"],
-//                                    inputLabelTextColor: json["inputLabelTextColor"])
         } catch AcceptSDKError.MissingArgumentError(let errorMessage) {
-//           print(errorMessage)
             self.errorCallback("-1",errorMessage, nil)
        }  catch let error {
-//           print(error.localizedDescription)
         self.errorCallback("-1", error.localizedDescription, error)
        }
 
 
     }
-    @objc func presentPayVC(vC: UIViewController, billingData: [String : String], paymentKey: String, saveCardDefault: Bool, showSaveCard: Bool, showAlerts: Bool, token: String? = nil, maskedPanNumber: String? = nil, buttonsColor: UIColor? = nil, isEnglish: Bool = true, backgroundColor: UIColor? = nil, navBarColor: UIColor? = nil, navBarTextColor: UIColor? = nil, textFieldBackgroundColor: UIColor? = nil, textFieldTextColor: UIColor? = nil, titleLabelTextColor: UIColor? = nil, inputLabelTextColor: UIColor? = nil, buttonText: String = "", cardNameLabelText: String = "", cardNumberLabelText: String = "", expirationLabelText: String = "", cvvLabelText: String = "") {
-
-        do {
-            try accept.presentPayVC(vC: vC,
-                                    billingData: billingData,
-                                    paymentKey: paymentKey,
-                                    saveCardDefault: saveCardDefault,
-                                    showSaveCard: showSaveCard,
-                                    showAlerts: showAlerts,
-                                    token: token,
-                                    maskedPanNumber: maskedPanNumber,
-                                    buttonsColor: buttonsColor,
-                                    isEnglish: isEnglish,
-                                    backgroundColor: backgroundColor,
-                                    navBarColor: navBarColor,
-                                    navBarTextColor: navBarTextColor,
-                                    textFieldBackgroundColor: textFieldBackgroundColor,
-                                    textFieldTextColor: textFieldTextColor,
-                                    titleLabelTextColor: titleLabelTextColor,
-                                    inputLabelTextColor: inputLabelTextColor)
-        } catch let error as NSError {
-                print("\(error.userInfo)")
-        }
-
-    }
+    
 
     public func paymentAttemptFailed(_ error: AcceptSDKError, detailedDescription: String) {
         self.successCallback([false, detailedDescription, error.localizedDescription])
-//        self.delegate?.rnPaymentAttemptFailed(error.localizedDescription, detailedDescription: detailedDescription)
     }
 
     public func transactionRejected(_ payData: PayResponse) {
-//        self.delegate?.rnTransactionRejected(payResponseToDictionary(payData: payData))
         self.successCallback([false, 400, "Transaction Rejected!!"])
     }
 
     public func transactionAccepted(_ payData: PayResponse) {
-//        self.delegate?.rnTransactionAccepted(payResponseToDictionary(payData: payData))
         self.successCallback([true, 0, "", ""])
     }
 
     public func transactionAccepted(_ payData: PayResponse, savedCardData: SaveCardResponse) {
-//        self.delegate?.rnTransactionAccepted(payResponseToDictionary(payData: payData), savedCardData: saveCardResponseToDictionary(savedCardData: savedCardData))
         self.successCallback([true, 0, "", savedCardData.token])
     }
 
     public func userDidCancel3dSecurePayment(_ pendingPayData: PayResponse) {
-//        self.delegate?.rnUserDidCancel3dSecurePayment(payResponseToDictionary(payData: pendingPayData))
         self.successCallback([false, 0, "User canceled 3-d secure verification!!"])
     }
 
     public func userDidCancel() {
-//        self.delegate?.rnUserDidCancel()
         self.successCallback([false, 0, "User canceled!!"])
     }
 
-    func saveCardResponseToDictionary(savedCardData: SaveCardResponse) -> [String : Any] {
-        return [
-            "card_subtype" : savedCardData.card_subtype,
-            "id" : savedCardData.id,
-            "token" : savedCardData.token,
-            "created_at" : savedCardData.created_at,
-            "masked_pan" : savedCardData.masked_pan,
-            "merchant_id" : savedCardData.merchant_id,
-            "order_id" : savedCardData.order_id as Any,
-            "email" : savedCardData.email as Any
-        ] as [String : Any]
-    }
-
-    func payResponseToDictionary(payData: PayResponse) -> [String : Any] {
-
-        return [
-            "amount_cents": payData.amount_cents,
-             "is_refunded": payData.is_refunded,
-             "is_capture": payData.is_capture,
-             "captured_amount": payData.captured_amount,
-             "source_data_type": payData.source_data_type,
-             "pending": payData.pending,
-             "is_3d_secure": payData.is_3d_secure,
-             "id": payData.id,
-             "is_void": payData.is_void,
-             "currency": payData.currency,
-             "is_auth": payData.is_auth,
-             "is_refund": payData.is_refund,
-             "owner": payData.owner,
-             "is_voided": payData.is_voided,
-             "source_data_pan": payData.source_data_pan,
-             "profile_id": payData.profile_id,
-             "success": payData.success,
-             "dataMessage": payData.dataMessage,
-             "source_data_sub_type": payData.source_data_sub_type,
-             "error_occured": payData.error_occured,
-             "is_standalone_payment": payData.is_standalone_payment,
-             "created_at": payData.created_at,
-             "refunded_amount_cents": payData.refunded_amount_cents,
-             "integration_id": payData.integration_id,
-             "order": payData.order
-        ] as [String : Any]
-
-    }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,13 @@
 import { NativeModules } from 'react-native';
 
+interface Paymob {
+  payWithNoToken: (
+    data: any,
+    successCallback: (data: any) => void,
+    errorCallback: (error: any) => void
+  ) => void;
+  presentPayVC: () => void;
+}
+
 const { Paymob } = NativeModules;
-export default Paymob;
+export default Paymob as Paymob;
