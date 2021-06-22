b555f9e9e683a137eea39018feb9769d8d8c7e4a
Ahmed Eid <a.eid@yandex.com>
2021-06-22 08:37:14 +0200

    test 4

--- android/src/main/java/com/reactnativepaymob/PaymobModule.java
+++ android/src/main/java/com/reactnativepaymob/PaymobModule.java
@@ -11,6 +11,7 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.BaseActivityEventListener;
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Promise;
 
 import com.paymob.acceptsdk.IntentConstants;
 import com.paymob.acceptsdk.PayActivity;
@@ -27,226 +28,45 @@ public class PaymobModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
-  static final int ACCEPT_PAYMENT_REQUEST = 10;
-
-  private static final String E_ACTIVITY_DOES_NOT_EXIST = "E_ACTIVITY_DOES_NOT_EXIST";
-  private Callback mSuccessCallback;
-  private Callback mErrorCallback;
-  private ReadableMap mParams;
-
-
-
-  private void startPayActivityNoToken(Activity currentActivity, HashMap data) {
-    Intent pay_intent = new Intent(currentActivity, PayActivity.class);
-    Boolean showSaveCard = (Boolean) data.get("showSaveCard");
-    Boolean saveCardDefault = (Boolean) data.get("saveCardDefault");
-    Boolean isEnglish = (Boolean) data.get("isEnglish");
-    Boolean hideActionBar = (Boolean) data.get("hideActionBar");
-    String language = "en";
-    if(isEnglish == false){
-      language = "ar";
-    }
-    putNormalExtras(pay_intent, data);
-    // Boolean to save the entered card details by default
-    pay_intent.putExtra(PayActivityIntentKeys.SAVE_CARD_DEFAULT, saveCardDefault);
-    // Boolean to display the save card checkbox
-    pay_intent.putExtra(PayActivityIntentKeys.SHOW_SAVE_CARD, showSaveCard);
-    pay_intent.putExtra(PayActivityIntentKeys.THEME_COLOR,0x80970073);
-    Intent secure_intent = new Intent(currentActivity, ThreeDSecureWebViewActivty.class);
-    secure_intent.putExtra("ActionBar",hideActionBar);
-    pay_intent.putExtra("language",language);
-    // String themeColor = (String) data.get("themeColor");
-    // if(themeColor != null){
-    // pay_intent.putExtra(PayActivityIntentKeys.THEME_COLOR, Integer.parseInt(themeColor, 16));
-    // }
-
-    currentActivity.startActivityForResult(pay_intent, ACCEPT_PAYMENT_REQUEST);
-}
-
-private void startPayActivityToken(Activity currentActivity, HashMap data) {
-    Intent pay_intent = new Intent(currentActivity, PayActivity.class);
-
-    putNormalExtras(pay_intent, data);
-    // replace this with your actual card token
-    pay_intent.putExtra(PayActivityIntentKeys.TOKEN, (String) data.get("token"));
-    pay_intent.putExtra(PayActivityIntentKeys.MASKED_PAN_NUMBER, (String) data.get("maskedPanNumber"));
-    currentActivity.startActivityForResult(pay_intent, ACCEPT_PAYMENT_REQUEST);
-}
-
-private void putNormalExtras(Intent intent, HashMap data) {
-    // Pass the correct values for the billing data keys
-    String firstName = (String) data.get("firstName");
-    if(firstName != null){
-      intent.putExtra(PayActivityIntentKeys.FIRST_NAME, firstName);
-    }
-    String lastName = (String) data.get("lastName");
-    if(lastName != null){
-      intent.putExtra(PayActivityIntentKeys.LAST_NAME, lastName);
-    }
-    String building = (String) data.get("building");
-    if(building != null){
-      intent.putExtra(PayActivityIntentKeys.BUILDING, building);
-    }
-    String floor = (String) data.get("floor");
-    if(floor != null){
-      intent.putExtra(PayActivityIntentKeys.FLOOR, floor);
-    }
-    String apartment = (String) data.get("apartment");
-    if(apartment != null){
-      intent.putExtra(PayActivityIntentKeys.APARTMENT, apartment);
-    }
-    String city = (String) data.get("city");
-    if(city != null){
-      intent.putExtra(PayActivityIntentKeys.CITY, city);
-    }
-    String state = (String) data.get("state");
-    if(state != null){
-      intent.putExtra(PayActivityIntentKeys.STATE, state);
-    }
-    String country = (String) data.get("country");
-    if(country != null){
-      intent.putExtra(PayActivityIntentKeys.COUNTRY, country);
-    }
-    String email = (String) data.get("email");
-    if(email != null){
-      intent.putExtra(PayActivityIntentKeys.EMAIL, email);
-    }
-    String phoneNumber = (String) data.get("phoneNumber");
-    if(phoneNumber != null){
-      intent.putExtra(PayActivityIntentKeys.PHONE_NUMBER, phoneNumber);
-    }
-    String postalCode = (String) data.get("postalCode");
-    if(postalCode != null){
-      intent.putExtra(PayActivityIntentKeys.POSTAL_CODE, postalCode);
-    }
-    String paymentKey = (String) data.get("paymentKey");
-    if(paymentKey != null){
-      intent.putExtra(PayActivityIntentKeys.PAYMENT_KEY, paymentKey);
-    }
-    String activityTitle = (String) data.get("activityTitle");
-    if(activityTitle!=null){
-      intent.putExtra(PayActivityIntentKeys.THREE_D_SECURE_ACTIVITY_TITLE, activityTitle);
-    }
-}
-
-private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
-  @Override
-  public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
-      // super.onActivityResult(requestCode, resultCode, data);
-      super.onActivityResult(activity, requestCode, resultCode, data);
-      try{
-        Bundle extras = data.getExtras();
-        boolean successStatus = false;
-        String token = "";
-        // Activity activity = getCurrentActivity();
-
-        // mErrorCallback.invoke(E_ACTIVITY_DOES_NOT_EXIST);
-        if (activity == null) {
-          mErrorCallback.invoke(E_ACTIVITY_DOES_NOT_EXIST);
-          return;
-        }
-        // mSuccessCallback.invoke(requestCode);
-        if (requestCode == ACCEPT_PAYMENT_REQUEST) {
-          String responseMessage = "";
-          if (resultCode == IntentConstants.USER_CANCELED) {
-            // User canceled and did no payment request was fired
-            responseMessage = "User canceled!!";
-          } else if (resultCode == IntentConstants.MISSING_ARGUMENT) {
-            // You forgot to pass an important key-value pair in the intent's extras
-            responseMessage = "Missing Argument == " + extras.getString(IntentConstants.MISSING_ARGUMENT_VALUE);
-          } else if (resultCode == IntentConstants.TRANSACTION_ERROR) {
-            // An error occurred while handling an API's response
-            responseMessage = "Reason == " + extras.getString(IntentConstants.TRANSACTION_ERROR_REASON);
-          } else if (resultCode == IntentConstants.TRANSACTION_REJECTED) {
-            // User attempted to pay but their transaction was rejected
-            // Use the static keys declared in PayResponseKeys to extract the fields you want
-            responseMessage = extras.getString(PayResponseKeys.DATA_MESSAGE);
-          } else if (resultCode == IntentConstants.TRANSACTION_REJECTED_PARSING_ISSUE) {
-            // User attempted to pay but their transaction was rejected. An error occured while reading the returned JSON
-            responseMessage = extras.getString(IntentConstants.RAW_PAY_RESPONSE);
-          } else if (resultCode == IntentConstants.TRANSACTION_SUCCESSFUL) {
-            // User finished their payment successfully
-            // Use the static keys declared in PayResponseKeys to extract the fields you want
-            responseMessage = extras.getString(PayResponseKeys.DATA_MESSAGE);
-            successStatus = true;
-          } else if (resultCode == IntentConstants.TRANSACTION_SUCCESSFUL_PARSING_ISSUE) {
-            // User finished their payment successfully. An error occured while reading the returned JSON.
-            responseMessage = "TRANSACTION_SUCCESSFUL - Parsing Issue";
-            successStatus = true;
-            // responseMessage = extras.getString(IntentConstants.RAW_PAY_RESPONSE);
-          } else if (resultCode == IntentConstants.TRANSACTION_SUCCESSFUL_CARD_SAVED) {
-          // User finished their payment successfully and card was saved.
-          // Use the static keys declared in PayResponseKeys to extract the fields you want
-          // Use the static keys declared in SaveCardResponseKeys to extract the fields you want
-            responseMessage = "Token == " + extras.getString(SaveCardResponseKeys.TOKEN);
-            successStatus = true;
-            token = extras.getString(SaveCardResponseKeys.TOKEN);
-          } else if (resultCode == IntentConstants.USER_CANCELED_3D_SECURE_VERIFICATION) {
-            responseMessage = "User canceled 3-d secure verification!!";
-            // Note that a payment process was attempted. You can extract the original returned values
-            // Use the static keys declared in PayResponseKeys to extract the fields you want
-            responseMessage = extras.getString(PayResponseKeys.PENDING);
-          } else if (resultCode == IntentConstants.USER_CANCELED_3D_SECURE_VERIFICATION_PARSING_ISSUE) {
-            responseMessage = "User canceled 3-d scure verification - Parsing Issue!!";
-            // Note that a payment process was attempted.
-            // User finished their payment successfully. An error occured while reading the returned JSON.
-            responseMessage = extras.getString(IntentConstants.RAW_PAY_RESPONSE);
-          }
-          // ToastMaker.displayShortToast(activity, responseMessage);
-          // HashMap resultMap  = new HashMap();
-          // resultMap.put("code", resultCode);
-          // resultMap.put("message", responseMessage);
-          mSuccessCallback.invoke(successStatus, resultCode, responseMessage, token);
-        }
-
-      }catch (Exception e){
-        mErrorCallback.invoke(e);
-      }
-  }
-};
-
   public PaymobModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    reactContext.addActivityEventListener(mActivityEventListener);
     this.reactContext = reactContext;
   }
 
 
-  //********************* React Methods ******************************/
-
   @ReactMethod
-  public void payWithToken(ReadableMap params, Callback successCallback, Callback errorCallback) {
-    Activity currentActivity = getCurrentActivity();
-    mSuccessCallback = successCallback;
-    mErrorCallback = errorCallback;
-    mParams = params;
+  public void present(ReadableMap params, Promise promise) {
+     try {
+        Activity currentActivity = getCurrentActivity();
+        Intent pay_intent = new Intent(currentActivity, PayActivity.class);
 
-    if (currentActivity == null) {
-      mErrorCallback.invoke(E_ACTIVITY_DOES_NOT_EXIST);
-      return;
+        pay_intent.putExtra(PayActivityIntentKeys.PAYMENT_KEY, "ZXlKMGVYQWlPaUpLVjFRaUxDSmhiR2NpT2lKSVV6VXhNaUo5LmV5SjFjMlZ5WDJsa0lqb3pOVFUxTENKdmNtUmxjbDlwWkNJNk5qRXpNams0TUN3aVkzVnljbVZ1WTNraU9pSkZSMUFpTENKcGJuUmxaM0poZEdsdmJsOXBaQ0k2TkRnMU9Dd2liRzlqYTE5dmNtUmxjbDkzYUdWdVgzQmhhV1FpT21aaGJITmxMQ0ppYVd4c2FXNW5YMlJoZEdFaU9uc2labWx5YzNSZmJtRnRaU0k2SWtOc2FXWm1iM0prSWl3aWJHRnpkRjl1WVcxbElqb2lUbWxqYjJ4aGN5SXNJbk4wY21WbGRDSTZJa1YwYUdGdUlFeGhibVFpTENKaWRXbHNaR2x1WnlJNklqZ3dNamdpTENKbWJHOXZjaUk2SWpReUlpd2lZWEJoY25SdFpXNTBJam9pT0RBeklpd2lZMmwwZVNJNklrcGhjMnR2YkhOcmFXSjFjbWRvSWl3aWMzUmhkR1VpT2lKVmRHRm9JaXdpWTI5MWJuUnllU0k2SWtOU0lpd2laVzFoYVd3aU9pSmpiR0YxWkdWMGRHVXdPVUJsZUdFdVkyOXRJaXdpY0dodmJtVmZiblZ0WW1WeUlqb2lLemcyS0RncE9URXpOVEl4TURRNE55SXNJbkJ2YzNSaGJGOWpiMlJsSWpvaU1ERTRPVGdpTENKbGVIUnlZVjlrWlhOamNtbHdkR2x2YmlJNklrNUJJbjBzSW1WNGNDSTZNell3TURBd01EQXdNREF3TVRZd01ESTJNakF3T1N3aVlXMXZkVzUwWDJObGJuUnpJam8xTURBd01EQXNJbkJ0YTE5cGNDSTZJalF4TGpJek5pNHhOREF1TWpFMEluMC5yTXdsRXhiWF8xbFZsVHBIanhwaFNscDlLd3V1YzVnRVBfRy1xTFgtdHRnWW83SWJQNXpsTDFfS2RlYkxUeGlXMHl6WEM2Z29LbnVoQVAxMjV5RWExUQ==");
+        pay_intent.putExtra(PayActivityIntentKeys.THREE_D_SECURE_ACTIVITY_TITLE, "Verification");
+        pay_intent.putExtra("language","ar");
+        pay_intent.putExtra(PayActivityIntentKeys.TOKEN, "6088c38c19705a495f1727561d4f4814b2ed7e45e9cd80c72f233253");
+        pay_intent.putExtra(PayActivityIntentKeys.MASKED_PAN_NUMBER, "xxxx-xxxx-xxxx-1234");
+        pay_intent.putExtra(PayActivityIntentKeys.SAVE_CARD_DEFAULT, false);
+        pay_intent.putExtra(PayActivityIntentKeys.SHOW_SAVE_CARD, false);
+        pay_intent.putExtra("ActionBar", true);
+
+        pay_intent.putExtra( PayActivityIntentKeys.FIRST_NAME, "Cliffo");
+        pay_intent.putExtra(PayActivityIntentKeys.LAST_NAME, "Nicol");
+        pay_intent.putExtra(PayActivityIntentKeys.BUILDING,"8028");
+        pay_intent.putExtra(PayActivityIntentKeys.FLOOR, "42");
+        pay_intent.putExtra(PayActivityIntentKeys.APARTMENT, "803");
+        pay_intent.putExtra(PayActivityIntentKeys.CITY, "Jask");
+        pay_intent.putExtra(PayActivityIntentKeys.STATE, "Uta");
+        pay_intent.putExtra(PayActivityIntentKeys.COUNTRY,"CR" );
+        pay_intent.putExtra(PayActivityIntentKeys.EMAIL, "claudette09@exa.com");
+        pay_intent.putExtra(PayActivityIntentKeys.PHONE_NUMBER, "+86(8)9135210487");
+        pay_intent.putExtra(PayActivityIntentKeys.POSTAL_CODE,  "01898");
+
+        currentActivity.startActivityForResult(pay_intent, 10);
+        promise.resolve(12345);
+    } catch(Exception e) {
+        promise.reject("Create Event Error", e);
     }
 
-    HashMap paramsMap = params.toHashMap();
-    try{
-      this.startPayActivityToken(currentActivity, paramsMap);
-    } catch(Exception e){
-      mErrorCallback.invoke(e);
-    }
-  }
-
-  @ReactMethod
-  public void payWithNoToken(ReadableMap params, Callback successCallback, Callback errorCallback) {
-    Activity currentActivity = getCurrentActivity();
-    mSuccessCallback = successCallback;
-    mErrorCallback = errorCallback;
-    mParams = params;
-
-    if (currentActivity == null) {
-      mErrorCallback.invoke(E_ACTIVITY_DOES_NOT_EXIST);
-      return;
-    }
-    HashMap paramsMap = params.toHashMap();
-    this.startPayActivityNoToken(currentActivity, paramsMap);
   }
 
   @Override
